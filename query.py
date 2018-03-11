17c698a881269b68387cd7491754afa2af0e7e77
PranavMudgil <pranavmudgil@gmail.com>
2018-03-12 04:24:58 +0530

    Add comments

--- query.py
+++ query.py
@@ -1,58 +1,59 @@
-import psycopg2,bleach
+import psycopg2
+import bleach
 
 
-#database variable globally declared
+# database variable globally declared
 DBNAME = "news"
 
-#Variable holding questions
-query_title1 = ("1. What are the most popular three articles of all time?")
-query_title2 = ("2. Who are the most popular article authors of all time?")
-query_title3 = ("3. On which days did more than 1% of requests lead to errors?")
+# Variable holding questions
+query_title1 = ("1.What are the most popular three articles of all time?")
+query_title2 = ("2.Who are the most popular article authors of all time?")
+query_title3 = ("3.On which days did more than 1% of requests lead to errors?")
 
-#First query
+# First query
 query1 = """select articles.title as Articles,count(*)
-             as Views from articles,log where log.path
-             like concat('%',articles.slug,'%') and 
-             log.status like '%200 OK%' group by
-             articles.title,path order by Views desc
-             limit 3"""
-#Second query
+            as Views from articles,log where log.path
+            like concat('%',articles.slug,'%') and 
+            log.status like '%200 OK%' group by
+            articles.title,path order by Views desc
+            limit 3"""
+# Second query
 query2 = """select authors.name as Author,count(*) as Views
-         from articles,log,authors where articles.author=authors.id
-         and log.path like concat('%',articles.slug,'%') and 
-         log.status like '%200%' group by authors.name,log.path order
-         by log.path desc limit 3"""
-#Third query
+            from articles,log,authors where articles.author=authors.id
+            and log.path like concat('%',articles.slug,'%') and 
+            log.status like '%200%' group by authors.name,log.path order
+            by log.path desc limit 3"""
+# Third query
 query3 = "select * from error_view where Error_Percentage > 1 "
 def answer_query(query):
-    #connecting to database
+    # connecting to database
     db = psycopg2.connect(database=DBNAME)
-    #Initiating cursor
+    # Initiating cursor
     c = db.cursor()
-    #Query execution
+    # Query execution
     c.execute(query)
-    #fetching contents of table
+    # fetching contents of table
     results = c.fetchall()
-    #Closing database
+    # Closing database
     db.close()
     return results
 
-#Function to print third query
+# Function to print third query
 def printerrorquery(results):
     print(query_title3)
-    #Loop to fetch and print values of result
+    # Loop to fetch and print values of result
     for i in results:
         Article = i[0]
         Views = i[1]
-        print("\t %s -- %.1f" %(Article,Views)+" % errors")
+        print("\t %s -- %.1f" % (Article, Views)+" % errors")
 
-#Function to print first and second query        
+# Function to print first and second query
 def printmequery(results,title):
     print(title)
     for i in results:
         Article = i[0]
         Views = i[1]
-        print('\t %s -- %d' %(Article,Views)+" views")
+        print('\t %s -- %d' %(Article, Views)+" views")
         
 result1 = answer_query(query1)
 printmequery(result1,query_title1)
