3357e9e8d20b19b335fd9de4f9b440930b4f97ee
PranavMudgil <pranavmudgil@gmail.com>
2018-03-12 13:12:20 +0530

    Correct pep-8 guidelines

--- query.py
+++ query.py
@@ -13,18 +13,20 @@ query_title3 = ("3.On which days did more than 1% of requests lead to errors?")
 # First query
 query1 = """select articles.title as Articles,count(*)
             as Views from articles,log where log.path
-            like concat('%',articles.slug,'%') and 
+            like concat('%',articles.slug,'%') and
             log.status like '%200 OK%' group by
             articles.title,path order by Views desc
             limit 3"""
 # Second query
 query2 = """select authors.name as Author,count(*) as Views
             from articles,log,authors where articles.author=authors.id
-            and log.path like concat('%',articles.slug,'%') and 
+            and log.path like concat('%',articles.slug,'%') and
             log.status like '%200%' group by authors.name,log.path order
             by log.path desc limit 3"""
 # Third query
 query3 = "select * from error_view where Error_Percentage > 1 "
+
+
 def answer_query(query):
     # connecting to database
     db = psycopg2.connect(database=DBNAME)
@@ -38,6 +40,7 @@ def answer_query(query):
     db.close()
     return results
 
+
 # Function to print third query
 def printerrorquery(results):
     print(query_title3)
@@ -45,21 +48,22 @@ def printerrorquery(results):
     for i in results:
         Article = i[0]
         Views = i[1]
-        print("\t %s -- %.1f" % (Article, Views)+" % errors")
+        print("\t %s -- %.1f" % (Article, Views) + " % errors")
+
 
 # Function to print first and second query
-def printmequery(results,title):
+def printmequery(results, title):
     print(title)
     for i in results:
         Article = i[0]
         Views = i[1]
-        print('\t %s -- %d' %(Article, Views)+" views")
-        
+        print('\t %s -- %d' % (Article, Views) + " views")
+
 result1 = answer_query(query1)
-printmequery(result1,query_title1)
+printmequery(result1, query_title1)
 print('\n')
 result2 = answer_query(query2)
-printmequery(result2,query_title2)
+printmequery(result2, query_title2)
 print('\n')
 results3 = answer_query(query3)
 printerrorquery(results3)
